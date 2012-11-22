0bc6e5456b4071d86fa73451a2263c6943730522
Tiago de França Queiroz <contato@tiago.eti.br>
2012-11-22 09:01:00 -0200

    tests

---
+++ src/tests/testing.java
@@ -0,0 +1,19 @@
+package tests;
+
+import geneticalgorithm.Chromosome;
+
+public class testing
+{
+	public static void main(String[] trash)
+	{
+		Chromosome c1, c2;
+		
+		c1 = new Chromosome();
+		c2 = new Chromosome();
+		
+		System.out.println(c1.getValue());
+		System.out.println(c2.getValue());
+		
+	}
+
+}

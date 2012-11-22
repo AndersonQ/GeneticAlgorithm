b86469341bd6c011252cd575417712082ab59e53
Anderson de França Queiroz <contato@andersonq.eti.br>
2012-11-22 08:58:09 -0200

    Useful scripts

---
+++ gitpull.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "==========Pushing origin master=========="
+git pull origin master
+echo "==========Pushing gitorious master======="
+git pull gitorious master
---
+++ gitpush.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "==========Pushing origin master=========="
+git push origin master
+echo "==========Pushing gitorious master======="
+git push gitorious master

d7d0c1ad1eb9d5da47485d433b2599a0748b1ee0
Fernando Henrique Gomes Zucatelli <fernandozucatelli@bol.com.br>
2012-11-24 17:15:24 -0200

    I tried to edit but i turned it back, sorry

--- src/tests/testing.java
+++ src/tests/testing.java
@@ -7,16 +7,15 @@ public class testing
 	public static void main(String[] trash)
 	{
 		Chromosome c1, c2;
-		
+
 		c1 = new Chromosome();
 		c2 = new Chromosome();
-		
+			
 		System.out.println(c1.getValue());
 		System.out.println(c2.getValue());
-		
+		 		
 		System.out.println(c1);
 		System.out.println(c2);
-		
 	}
 
 }

2257850ee6d3f35c8bf47885f74a5694a1d7ac9e
Fernando Henrique Gomes Zucatelli <fernandozucatelli@bol.com.br>
2012-11-25 12:58:59 -0200

    Adding selection of rates to main as comment
    There is an explanation of my choice to add it as comment.

--- src/tests/testing.java
+++ src/tests/testing.java
@@ -1,5 +1,7 @@
 package tests;
 
+import java.util.Scanner;
+
 import geneticalgorithm.Chromosome;
 
 public class testing
@@ -19,3 +21,92 @@ public class testing
 	}
 
 }
+
+/*
+ * Sorry guys, I made it to select somes rates at the main flow of the program
+ * but I was afraid to make any mistake with the main class
+ * so I decided to insert it as a comment block, which I believe is less dangerous than
+ * any other change I could make. I teste id and it worked, as simple as it is.
+ * 
+import java.util.Scanner;
+
+public class testing
+{
+	public static void main(String[] trash)
+	{
+		System.out.println("Algoritmo Genético para pontos ótimos de funções");
+
+		int op = 100, cycle=1, i;
+		double txmutation=0, txelite=0, txcross=0;
+		Scanner sc = new Scanner(System.in);
+
+		while(op > 0)
+		{
+
+			while (txcross < 1 || txcross > 80) {
+				System.out.printf("\nEscolha a taxa de crossover , real de 1 a 80 :\n");
+				System.out.printf("> ");
+				txcross = sc.nextInt();
+			}
+
+			while (txmutation <= 0 || txmutation > 80) {
+				System.out.printf("\nEscolha a taxa de mutação, real de 0 a 80 :\n");
+				System.out.printf("> ");
+				txmutation = sc.nextDouble();
+			}
+
+			while (txelite <= 0 || txmutation > 80) {
+				System.out.printf("\nEscolha a taxa de elitismo, real de 0 a 80 :\n");
+				System.out.printf("> ");
+				txelite = sc.nextDouble();
+			}
+
+			System.out.printf("0 - Sair\n");
+			System.out.printf("1 - Maximizar função\n");
+			System.out.printf("2 - Minimizar função\n");
+
+			System.out.printf("> ");
+			op = sc.nextInt();            
+
+			switch(op)
+			{
+			case 1:
+
+				while (cycle > 0) {
+					System.out.printf("\n\nMaximizar função escolhido\n");
+					System.out.printf("\nQuantos ciclos rodar? 0 => Sair:\n");
+					System.out.printf("> ");
+					cycle = sc.nextInt();
+
+					for (i=1; i <= cycle; i++){
+						System.out.printf("\nGeração %d", i);
+					}
+						
+				}
+				//break;
+				
+			case 2:
+
+				while (cycle > 0) {
+					System.out.printf("\n\nMinimizar função escolhido\n ");
+					System.out.printf("\nQuantos ciclos rodar? 0 => Sair:\n");
+					System.out.printf("> ");
+					cycle = sc.nextInt();
+					
+					for (i=1; i <= cycle; i++){
+						System.out.printf("\nGeração %d", i);
+					}
+										
+				}
+				//break;
+								
+			//default:
+				//break;
+			}
+			// Seting variables to reenter while loopings
+			txmutation=0; txelite=0; txcross=0; cycle=1;
+		}
+	}
+}
+
+*/

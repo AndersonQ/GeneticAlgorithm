e800e6631b3bee218bfc89df7d4ed5412e17a964
Tiago de França Queiroz <contato@tiago.eti.br>
2012-12-01 22:34:13 -0200

    Little fixs and code cleaning

--- src/tests/AG.java
+++ src/tests/AG.java
@@ -16,26 +16,26 @@ public class AG
         double elitep = 0.020;
         double total, r;
         int pop = 10;
-        int cycles = 50;
+        int cycles = 8;
         int i, k, j;
         int elite = (int) Math.floor(pop*(1 - elitep));
         Chromosome o_pop[], n_pop[], p1, p2;
-        
+
         o_pop = new Chromosome[pop];
         n_pop = new Chromosome[pop];
-        
+
         System.out.printf("Initializing....\n");
         for(i = 0; i < pop; i++)
         {
             o_pop[i] = new Chromosome();
             System.out.printf("[%d] %s: %f\n", i, o_pop[i], o_pop[i].getValue());
         }
-        
-        for(i = 0; i < pop; i++)
+
+        for(i = 0; i < cycles; i++)
         {
             System.out.printf("==================Generation %d==================\nEvaluating....\n", i);
             total = 0;
-            
+
             /* Evaluate */
             for(k = 0; k < pop; k++)
             {
@@ -44,26 +44,23 @@ public class AG
                 System.out.printf("[%d] = %f\n", k, o_pop[k].getRank());
             }
             System.out.printf("Total = %f\n", total);
-            
+
             /* Calculate the percentage of fitness and store it in Rank */
             for(k = 0; k < pop; k++)
-            {
-//                System.out.printf("[%d] %f%%\n", k, o_pop[k].getRank()/total);
                 o_pop[k].setRank(o_pop[k].getRank()/total);
-            }
-          
+
             /* Sort the vector using Rank*/
             Arrays.sort(o_pop);
-            
+
             /* Print the percentage of fitness */
             System.out.printf("Percentage of fitness\n");
             for(k = 0; k < pop; k++)
                 System.out.printf("[%d] %f%%\n", k, o_pop[k].getRank());
-            
+
             /* Put the elite in the new poupation vector */
             for(k = elite; k < pop; k++)
                 n_pop[k] = o_pop[k];
-            
+
             /*
              * For the rest of the new population:
              *  - Select two chromosomes
@@ -76,36 +73,30 @@ public class AG
                 /* Select p1 */
                 r = Math.random();
                 for(j = 0; j < pop; j++)
-                {
-//                    System.out.printf("%f <= %f: %s\n", r, o_pop[j].getRank(), r <= o_pop[j].getRank());
                     if(r <= o_pop[j].getRank())
                         break;
-                }
+
                 if(j == pop)
                     j = pop - 1;
                 p1 = o_pop[j];
-//                System.out.printf("[%d]P1 = %d r = %f\n", k, j, r);
-                
+
                 /* Select p2 */
                 r = Math.random();
                 for(j = 0; j < pop; j++)
-                {
-//                    System.out.printf("%f <= %f: %s\n", r, o_pop[j].getRank(), r <= o_pop[j].getRank());
                     if(r <= o_pop[j].getRank())
                         break;
-                }
+
                 if(j == pop)
                     j = pop - 1;
                 p2 = o_pop[j];
-//                System.out.printf("[%d]P2 = %d, r = %f\n", k,  j, r);
-                
+
                 /* Avoid Index out of bounds */
                 if(p1 == p2)
                     if(j != 0)
                         p2 = o_pop[--j];
                     else
                         p2 = o_pop[++j];
-                
+
                 Chromosome cross[] = Chromosome.crossover(p1, p2);
                 n_pop[k] = cross[0];
                 n_pop[k].mutation(mutation);
@@ -117,9 +108,9 @@ public class AG
     public static double evaluate(Chromosome c)
     {
         double v, x = c.getValue();
-        
+
         v = 50000000000000000000.01*Math.pow(x, -5.0)/(Math.exp(100.0/x) - 1.0);
-        
+
         return v;
     }
 }

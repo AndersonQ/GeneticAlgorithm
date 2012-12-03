6d949d4fedb332e93580d615ba2cb73152203b7d
Tiago de França Queiroz <contato@tiago.eti.br>
2012-12-03 18:39:50 -0200

    a comparator to do the contest

---
+++ src/geneticalgorithm/CompMaximize.java
@@ -0,0 +1,18 @@
+package geneticalgorithm;
+
+import java.util.Comparator;
+
+public class CompMaximize implements Comparator<Chromosome>
+{
+
+    public int compare(Chromosome c1, Chromosome c2)
+    {
+        if(c1.getRank() > c2.getRank())
+            return -1;
+        else if(c1.getRank() < c2.getRank())
+            return 1;
+        else
+            return 0;
+    }
+
+}

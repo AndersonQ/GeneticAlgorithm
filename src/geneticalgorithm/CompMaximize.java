85524a459efc24d0e68a366d3c98f0546a2cac7a
Anderson de França Queiroz <contato@andersonq.eti.br>
2012-12-03 22:11:48 -0200

    LICENSE-LGPL

--- src/geneticalgorithm/CompMaximize.java
+++ src/geneticalgorithm/CompMaximize.java
@@ -1,7 +1,30 @@
+/*
+ * Copyright 2012 Anderson Queiroz <contato(at)andersonq(dot)eti(dot)br>
+ * 					Fernando Zucatelli, João Coutinho, Tiago Queiroz <contato(at)tiago(dot)eti(dot)br>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package geneticalgorithm;
 
 import java.util.Comparator;
 
+/**
+ * 
+ * @author Anderson Queiroz, Fernando Zucatelli, João Coutinho, Tiago Queiroz
+ *
+ */
 public class CompMaximize implements Comparator<Chromosome>
 {
     public int compare(Chromosome c1, Chromosome c2)
